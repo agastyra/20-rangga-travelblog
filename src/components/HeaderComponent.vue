327973a2bf7df3e5cef855bf5bb2f35bc939cec8
Rangga Agastya <rangga.agastya711@gmail.com>
2022-03-21 16:47:17 +0700

    komponen header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,35 +1,17 @@
 <template>
-  <v-app-bar app color="primary" dark>
-    <div class="d-flex align-center">
-      <v-img
-        alt="Vuetify Logo"
-        class="shrink mr-2"
-        contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-        transition="scale-transition"
-        width="40"
-      />
-
-      <v-img
-        alt="Vuetify Name"
-        class="shrink mt-1 hidden-sm-and-down"
-        contain
-        min-width="100"
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-        width="100"
-      />
+  <v-app-bar style="background: #adbfcd" elevate-on-scroll app>
+    <div class="container">
+      <v-row>
+        <v-img src="@/assets/logo.png" max-width="50" max-height="50"></v-img>
+        <v-spacer></v-spacer>
+        <ul class="d-flex">
+          <li>Home</li>
+          <li>Blogs</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </v-row>
     </div>
-
-    <v-spacer></v-spacer>
-
-    <v-btn
-      href="https://github.com/vuetifyjs/vuetify/releases/latest"
-      target="_blank"
-      text
-    >
-      <span class="mr-2">Latest Release</span>
-      <v-icon>mdi-open-in-new</v-icon>
-    </v-btn>
   </v-app-bar>
 </template>
 
@@ -37,4 +19,17 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+ul {
+  list-style: none;
+  align-items: center;
+}
+
+li {
+  margin: 0 15px;
+}
+
+li:last-child {
+  margin-right: 0;
+}
+</style>
