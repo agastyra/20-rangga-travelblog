12bb3912a1545fbb6a0810550127735eefd24c91
Rangga Agastya <rangga.agastya711@gmail.com>
2022-03-21 16:41:10 +0700

    menambahkan komponen

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,37 @@
   <v-app>
     <VHeader />
     <v-main>
-      <router-view></router-view>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
     </v-main>
+    <Contact />
+    <VFooter />
   </v-app>
 </template>
 
 <script>
 import VHeader from "@/components/HeaderComponent.vue";
+import VFooter from "@/components/FooterComponent.vue";
+import Contact from "@/components/ContactComponent.vue";
 
 export default {
   name: "App",
   components: {
     VHeader,
+    Contact,
+    VFooter,
   },
   data: () => ({
     //
   }),
 };
 </script>
+
+<style>
+.overlay {
+  width: 100%;
+  height: 100%;
+  background: rgba(92, 92, 92, 0.32);
+}
+</style>
