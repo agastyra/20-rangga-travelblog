6673de885c1d1b446e3bf7c6996ac0d8dafed54c
Rangga Agastya <rangga.agastya711@gmail.com>
2022-03-21 16:47:31 +0700

    komponen hero image

---
+++ src/components/HeroImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-img src="@/assets/1.jpg" style="position: relative">
+    <div class="overlay"></div>
+    <div class="hero-text">
+      <h1 class="">The Art Of Travel</h1>
+      <p>Finding beauty, one flight at a time</p>
+      <v-btn color="yellow" style="padding: 20px 25px">Subscribe</v-btn>
+    </div>
+  </v-img>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.hero-text {
+  color: white;
+  position: absolute;
+  top: 30%;
+  left: 45%;
+}
+
+.hero-text > * {
+  margin: 5px 0;
+}
+</style>
