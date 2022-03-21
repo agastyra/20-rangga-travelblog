12bbe812d778588ee8d97a505c6184cf47f3a7af
Rangga Agastya <rangga.agastya711@gmail.com>
2022-03-21 16:45:13 +0700

    komponen kontak

---
+++ src/components/ContactComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="contact">
+    <div class="d-flex container">
+      <div class="subscribe">
+        <h1>Subscribe</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam alias
+          sapiente vitae quidem eligendi explicabo possimus voluptates minus
+          labore iusto.
+        </p>
+        <form action="" method="post">
+          <input type="text" />
+          <button>Subscribe</button>
+        </form>
+      </div>
+      <div class="sosmed">
+        <ul>
+          <li>Email</li>
+          <li>Address</li>
+          <li>Phone</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.contact {
+  background: #eef0c4;
+}
+
+.contact > .container {
+  padding: 60px 0;
+}
+
+.subscribe {
+  width: 50vw;
+}
+</style>
